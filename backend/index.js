de42e2be0a6f84d49d43d791601f91edc3c7c50c
mohd-asad17 <asadn01702@gmail.com>
2025-05-17 23:36:15 +0530

    adding zod validation

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-console.log("hello there");
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/todo', function(req, res){
+
+});
+
+app.get('/todos', function(req, res){
+
+});
+
+app.put('/completed', function(req, res){
+
+});
