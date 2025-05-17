f2d2b1ec97cf95e281c7d1f2c12dd9c8db2a0efe
mohd-asad17 <asadn01702@gmail.com>
2025-05-18 00:01:04 +0530

    post and put routes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express();
 
 app.use(express.json());
 
 app.post('/todo', function(req, res){
-
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: " you sent the wrong inputs"
+        })
+        return;
+    }
+    // put it in mongodb
 });
 
 app.get('/todos', function(req, res){
@@ -12,5 +21,12 @@ app.get('/todos', function(req, res){
 });
 
 app.put('/completed', function(req, res){
-
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg : " you sent the wrong inputs"
+        })
+        return;
+    }
 });
